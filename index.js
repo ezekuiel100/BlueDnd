c1b33ee48fa511f15b140e2b64bbccc0c656be67
unknown <ezekuiel100@gmail.com>
2024-11-18 19:28:27 -0300

    Export

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as Draggable } from "./lib/Draggable";
-export { default as DropableArea } from "./lib/DropableArea";
+export { default as Draggable } from "./dist/Draggable";
+export { default as DropableArea } from "./dist/DropableArea";
